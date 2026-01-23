09a018eab6350f7deb69cff7b1ee5025d0b09549
Remy van Berkum <r.van.berkum@enterprisemodules.com>
2026-01-23 19:37:52 +0100

    Cleanup rcu schema on initial run

--- modules/role/manifests/osb/admin_server.pp
+++ modules/role/manifests/osb/admin_server.pp
@@ -1,9 +1,8 @@
 # Install java, WebLogic and create the domain.
-class role::osb::admin_server()
-{
-  contain ::profile::base
-  contain ::wls_profile::admin_server
+class role::osb::admin_server () {
+  contain profile::base
+  contain wls_profile::admin_server
 
-  Class['::profile::base'] -> Class['::wls_profile::admin_server']
-
+  Class['profile::base']
+  -> Class['wls_profile::admin_server']
 }
--- modules/role/manifests/osb/node.pp
+++ modules/role/manifests/osb/node.pp
@@ -1,10 +1,8 @@
 # A regular Fusion managed server
-class role::osb::node()
-{
-  contain ::profile::base
-  contain ::wls_profile::node
+class role::osb::node () {
+  contain profile::base
+  contain wls_profile::node
 
-  Class['::profile::base'] -> Class['::wls_profile::node']
+  Class['profile::base']
+  -> Class['wls_profile::node']
 }
-
- 
